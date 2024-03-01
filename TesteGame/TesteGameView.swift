e9756464bed4b3bf63f3449110a961216969baaa
Patrick Alves <patrickalvesarts@gmail.com>
2024-03-01 20:06:02 -0300

    Objects test

---
+++ TesteGame/TesteGameView.swift
@@ -0,0 +1,12 @@
+//
+//  TesteGameView.swift
+//  TesteGame
+//
+//  Created by Patrick Alves on 01/03/24.
+//
+
+import UIKit
+
+class TesteGameView: SKScene {
+
+}
---
+++ TesteGame/TesteGameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TesteGameViewController.swift
+//  TesteGame
+//
+//  Created by Patrick Alves on 01/03/24.
+//
+
+import UIKit
+
+class TesteGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
