92e1e6765aa052521f9c009f75dc9a40912f1b26
Patrick Alves <patrickalvesarts@gmail.com>
2024-03-02 21:35:39 -0300

    node

--- TesteGame/TesteGameViewController.swift
+++ TesteGame/TesteGameViewController.swift
@@ -6,24 +6,38 @@
 //
 
 import UIKit
+import SpriteKit
 
 class TesteGameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if let view = self.view as! SKView? {
+            
+            if   let cena = SKScene(fileNamed: "TesteGame.sks") {
+                cena.scaleMode = .aspectFill
+                view.presentScene(cena)
+            }
+            
+            view.ignoresSiblingOrder = true
+            view.showsFPS = true
+            view.showsPhysics = true
+            view.showsNodeCount = true
+            
+            // Do any additional setup after loading the view.
+        }
+        
+        
+        /*
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+         }
+         */
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
